a0b0fb7cb2e7731df4310c65c002ed1cb85a7b30
Ebrahim <122838783+EbrahimM78@users.noreply.github.com>
2024-03-28 22:11:32 +0000

    0x03

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,63 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+'''
+This script gets the standard input and outputs a statistics
+in every ten lines or when a keyboard interupt input is executed.
+'''
 
 import sys
 
+counter = 0
+counter_dict = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+    }
 
-i = 0
-sum_file_size = 0
-status_code = {'200': 0,
-               '301': 0,
-               '400': 0,
-               '401': 0,
-               '403': 0,
-               '404': 0,
-               '405': 0,
-               '500': 0}
+total_size = 0
+
+
+def printer():
+    '''
+    This function prints the output of
+    the statistics accumulated.
+    '''
+    print(f"File size: {total_size}")
+    for status, numReq in counter_dict.items():
+        if numReq > 0:
+            print(f"{status}: {numReq}")
+
 
 try:
     for line in sys.stdin:
-        args = line.split(' ')
-        if len(args) > 2:
-            status_line = args[-2]
-            file_size = args[-1]
-            if status_line in status_code:
-                status_code[status_line] += 1
-            sum_file_size += int(file_size)
-            i += 1
-            if i == 10:
-                print('File size: {:d}'.format(sum_file_size))
-                sorted_keys = sorted(status_code.keys())
-                for key in sorted_keys:
-                    value = status_code[key]
-                    if value != 0:
-                        print('{}: {}'.format(key, value))
-                i = 0
-except Exception:
-    pass
-finally:
-    print('File size: {:d}'.format(sum_file_size))
-    sorted_keys = sorted(status_code.keys())
-    for key in sorted_keys:
-        value = status_code[key]
-        if value != 0:
-            print('{}: {}'.format(key, value))
+        line_tup = line.split()
+        counter += 1
+
+        # if len(line_tup) == 9:
+        try:
+            file_size = int(line_tup[-1])
+            total_size += file_size
+        except (IndexError, ValueError, TypeError):
+            continue
+
+        try:
+            status_code = line_tup[-2]
+            if status_code in counter_dict.keys():
+                counter_dict[status_code] = counter_dict.get(
+                    status_code) + 1
+            else:
+                continue
+        except (IndexError, ValueError, TypeError):
+            continue
+
+        if counter == 10:
+            printer()
+            counter = 0
+    printer()
+except KeyboardInterrupt:
+    printer()
+    raise
