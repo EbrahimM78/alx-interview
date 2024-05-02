569b803158e2a34207d233c09c51c7cec5b80ebb
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2024-05-02 22:06:46 +0000

    0x08

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
-"""Making Change Problem"""
-
-
 def makeChange(coins, total):
-    """Making Changes"""
     if total <= 0:
         return 0
-<<<<<<< HEAD
-    check = 0
-    temp = 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
     coins.sort(reverse=True)
+    coin_count = 0
     for i in coins:
-        while check < total:
-            check += i
-            temp += 1
-        if check == total:
-            return temp
-        check -= i
-        temp -= 1
-    return -1
-=======
-    elif total > 0:
-        newList = sorted(coins[:])
-        newList = list(reversed(newList))
-        count = 0
-        value = total + 0
-        index = 0
-        while value >= 0 and (index < len(newList)):
-            if value >= newList[index]:
-                value = value - newList[index]
-                count += 1
-            elif value < newList[index]:
-                index += 1
-        if index == len(newList):
-            if value != 0:
-                return -1
-            elif value == 0:
-                return count
->>>>>>> 2b404ccac7c9730aacb28b2021fe6548171359d5
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
